6b5b9e48880e185fd534603a9904376a427a3f66
Painkiller-69 <128809931+Painkiller-69@users.noreply.github.com>
2023-06-01 22:01:49 +0530

    app.ts

--- app.ts
+++ app.ts
@@ -1,37 +1,32 @@
 interface Book {
-    title: string;
-    author: string;
-  }
-  
-  const books: Book[] = [
-    {"title":"My Talented book","author":"ABC"},
-    {"title":"My Third Book","author":"XYZ"},
-    {"title":"First Book","author":"CDE"}
-  ];
-  
-  // Get references to the input field and the results div
-  const searchInput = document.getElementById('searchInput') as HTMLInputElement;
-  const resultsDiv = document.getElementById('resultsDiv');
-  
-  // Event handler for the input field change event
-  searchInput.addEventListener('input', function() {
-    const searchTerm = searchInput.value.toLowerCase();
-  
-    // Clear previous results
-    if (resultsDiv) {
-      resultsDiv.innerHTML = '';
-    }
-  
-    // Search for books matching the entered term
-    const matchingBooks = books.filter(book => book.title.toLowerCase().includes(searchTerm) && book.title !== "First Book");
-  
-    // Display matching books
-    if (resultsDiv) {
-      matchingBooks.forEach(book => {
-        const bookInfo = document.createElement('p');
-        bookInfo.textContent = `${book.title} - ${book.author}`;
-        resultsDiv.appendChild(bookInfo);
-      });
+  title: string;
+  author: string;
+}
+
+const books: Book[] = [
+  { title: "My Talented book", author: "ABC" },
+  { title: "My Third Book", author: "XYZ" },
+  { title: "First Book", author: "CDE" },
+];
+
+function searchBooks(): void {
+  const searchInput: HTMLInputElement = document.getElementById(
+    "searchInput"
+  ) as HTMLInputElement;
+  const searchResults: HTMLElement = document.getElementById(
+    "searchResults"
+  ) as HTMLElement;
+  const searchTerm: string = searchInput.value;
+
+  // Clear previous search results
+  searchResults.innerHTML = "";
+
+  // Display the corresponding divs for matching books
+  books.forEach((book: Book) => {
+    if (book.title.includes(searchTerm)) {
+      const div: HTMLDivElement = document.createElement("div");
+      div.textContent = `${book.title} - ${book.author}`;
+      searchResults.appendChild(div);
     }
   });
-  
+}
